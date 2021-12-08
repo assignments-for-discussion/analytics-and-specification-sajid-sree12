29e9d62a1f19f2d180e8bc0cd848e0aae9d371fb
Sajid Dudekula <87434484+sajid-sree12@users.noreply.github.com>
2021-12-08 20:15:43 +0530

    Update average.js
    small change

--- average.js
+++ average.js
@@ -1,6 +1,6 @@
 
 function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+  return numbers.reduce((p, c)=> p + c, 0.01) / numbers.length;
 }
 
 module.exports = {average};
