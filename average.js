fbe06a6b63abf246287645b8a9e6d616b296670f
Sajid Dudekula <87434484+sajid-sree12@users.noreply.github.com>
2021-12-19 01:50:53 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,16 +1,10 @@
-var inputsArray = document.getElementsByTagName('input');
-
-function computeTotal() {
-  var tot = 0;
-  tot += parseFloat(inputsArray[0].value);
-  tot += parseFloat(inputsArray[1].value);
-  tot += parseFloat(inputsArray[2].value);
-  inputsArray[3].value/inputsArray.length = tot;
-}
-<input type="text"></input>
-<input type="text"></input>
-<input type="text"></input>
-<input type="text" disabled></input>
-<button type="button" onclick="computeTotal()">Calculate</button>
-
-module.exports = {computeTotal()};
+var start = parseInt(prompt("Enter the Starting Number"));
+var end = parseInt(prompt("Enter the Ending Number"));
+var total = 0;
+var count = (end - start) + 1
+var average = 0;
+console.log("Average of Numbers between " + start + " and " + end + " is: " );
+for (start = start; start <= end; start++)
+    total = total + start;
+average = total / count;
+module.exports = {average};
