560463772f20aeab3cac2fbaace624f47d8fd792
Sajid Dudekula <87434484+sajid-sree12@users.noreply.github.com>
2021-12-19 01:53:29 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,10 +1,5 @@
-var start = parseInt(prompt("Enter the Starting Number"));
-var end = parseInt(prompt("Enter the Ending Number"));
-var total = 0;
-var count = (end - start) + 1
-var average = 0;
-console.log("Average of Numbers between " + start + " and " + end + " is: " );
-for (start = start; start <= end; start++)
-    total = total + start;
-average = total / count;
+function average(numbers) {
+  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+}
+
 module.exports = {average};
