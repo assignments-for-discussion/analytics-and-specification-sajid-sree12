32da9c654617b619eb3957c5dee15c89cea756fd
Sajid Dudekula <87434484+sajid-sree12@users.noreply.github.com>
2021-12-19 01:46:25 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,8 +1,16 @@
-var i=0;
-function average(numbers) {
-   for (i=0;i<=numbers.legeth;i++)
-      if (isNaN(numbers[i])) numbers[i]= 0;
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
-}
+var inputsArray = document.getElementsByTagName('input');
 
-module.exports = {average};
+function computeTotal() {
+  var tot = 0;
+  tot += parseFloat(inputsArray[0].value);
+  tot += parseFloat(inputsArray[1].value);
+  tot += parseFloat(inputsArray[2].value);
+  inputsArray[3].value/inputsArray.length = tot;
+}
+<input type="text"></input>
+<input type="text"></input>
+<input type="text"></input>
+<input type="text" disabled></input>
+<button type="button" onclick="computeTotal()">Calculate</button>
+
+module.exports = {computeTotal()};
