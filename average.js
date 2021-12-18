b09b5e7bc6f353b4d31079d199868c30535989fc
Sajid Dudekula <87434484+sajid-sree12@users.noreply.github.com>
2021-12-19 01:36:22 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,5 +1,5 @@
+var i=0;
 function average(numbers) {
-   var i=0;
    for (i=0;i<=numbers.legeth;i++)
       if (isNaN(numbers[i])) numbers[i]= 0;
   return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
